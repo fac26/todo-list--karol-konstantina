afe2a7ceac8132706933983ace37f857039c3452
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-11-07 11:06:10 +0000

    debug mode functionality

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+//debug mode - set to true to see console.log messages
+
+// const debugButton = document.querySelector("#debug-button");
+// let debugMode = false;
+
+// debugButton.addEventListener("click", function(event) {
+//   //on debug button click, toggle debug mode
+//   if (debugMode) {
+//     debugMode = false;
+//     debugButton.innerHTML = "Debug Mode: Off";
+//   } else {
+//     debugMode = true;
+//     debugButton.innerHTML = "Debug Mode: On";
+//   }
+// });
+
+// //function to log messages to the console only in debug mode
+// //use instead of console.log in testing
+// function log(message) {
+//   if (debugMode) {
+//     console.log(message);
+//   }
+// }
