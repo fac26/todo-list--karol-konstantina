4185778aa809f459c316f80854d6af32113b6623
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-11-15 01:41:32 +0000

    lots of annotations and major refactor

--- scripts/script.js
+++ scripts/script.js
@@ -5,87 +5,121 @@ const taskList = document.querySelector("ul");
 
 let debugMode = false;
 
-// <====== Functions ======>
+// <====== FUNCTIONS ======>
 
-// <--- addTask function (add a task to the list) --->
-function addTask(string) {
-	const task = input.value;
-	if (!task) return;
+// <--- Create a new list item with elements --->
+function createListItem(task) {
+	// <- Create the list item ->
 	const listItem = document.createElement("li");
 	const listText = document.createElement("p");
 	listText.innerText = task;
 	listItem.append(listText);
-	taskList.append(listItem);
-	input.value = "";
 
-	// <--- Create buttons --->
-	const deleteButton = buttonFactory({ id: "delete", text: "Delete" }, deleteTask, listItem);
-	const completeButton = buttonFactory({ id: "complete", text: "Complete" }, completeTask, listItem);
-	const urgentButton = buttonFactory({ id: "urgent", text: "Urgent" }, urgentTask, listItem);
-	const editButton = buttonFactory({ id: "edit", text: "Edit" }, editTask, listItem);
+	// <- Declare buttons ->
+	const buttons = [
+		{ id: "delete", text: "Delete", func: deleteTask },
+		{ id: "complete", text: "Complete", func: completeTask },
+		{ id: "urgent", text: "Urgent", func: urgentTask },
+		{ id: "edit", text: "Edit", func: editTask },
+	];
 
-	return task;
-	// unsure {
-	// create and append delete button to the new Task
-	// create and append complete button to the new Task
-	// create and append urgent button to the new Task
-	// create and append edit button to the new Task
-	// }
-	// create list item
-	// append list item to list
-	// clear input
-}
-
-// <--- deleteTask function (delete a task from the list) --->
-const deleteTask = (task) => {
-	task.remove();
-};
-
-// <--- completeTask function (complete a task) --->
-function completeTask(task) {
-	// add a strike-through to the parent task
-	// add id to the parent task to indicate it is complete
-	// add a class to the task to indicate it is complete
-}
-
-// <--- urgentTask function (make a task urgent) --->
-function urgentTask(task) {
-	// add a class to the task to indicate it is urgent
-}
-
-// <--- editTask function (edit a task) --->
-function editTask(task) {
-	//convert the task to an input field
-	//add a save button to the task
-	//add a cancel button to the task
-}
-
-// <===== Event Listeners ======>
-
-// <--- addTask listener --->
-addButton.addEventListener("click", (e) => {
-	e.preventDefault();
-	input.value !== "" ? addTask() : alert("No task entered");
-});
-
-// <--- debugMode listener --->
-debugButton.addEventListener("click", (e) => {
-	debugMode ? (debugMode = false) : (debugMode = true);
-	debugMode ? (debugButton.innerText = "Debug Mode: On") : (debugButton.innerText = "Debug Mode: Off");
-});
-
-// <====== Factories! ======>
-
-// function taskFactory() {
-// }
-
-function buttonFactory({ id, text }, callback, parent) {
-	const button = document.createElement("button");
-	[button.id, button.innerText] = [id, text];
-	parent.append(button);
-	button.addEventListener("click", (e) => {
-		callback(parent);
+	// <- Add buttons to the new task ->
+	buttons.forEach((button) => {
+		const buttonElement = buttonFactory(button);
+		listItem.append(buttonElement);
 	});
 
+	return listItem;
+}
+
+// <--- Add a task to the corresponding listItem --->
+function addTask(task) {
+	task = input.value;
+	const listItem = createListItem(task);
+	taskList.append(listItem);
+	input.value = "";
+}
+
+// <--- Delete the task --->
+const deleteTask = (task) => task.remove();
+
+// <--- Mark the task as completed --->
+function completeTask(task) {
+	task.classList.toggle("completed");
+
+	// <- Move the task to the bottom of the list ->
+	taskList.append(task);
+}
+
+// <--- Mark the task as urgent --->
+function urgentTask(task) {
+	task.classList.toggle("urgent");
+
+	// <- Move the task to the top of the list ->
+	taskList.prepend(task);
+}
+
+// <--- Edit the task --->
+function editTask(task) {
+	// <- Convert the task to an input field ->
+	const taskText = task.querySelector("p");
+	const taskInput = document.createElement("input");
+	taskInput.value = taskText.innerText;
+	task.replaceChild(taskInput, taskText);
+	taskInput.focus();
+	// Add a save button
+	// Add a cancel button
+}
+
+// <===== EVENT LISTENERS ======>
+
+// <--- Add button listener --->
+addButton.addEventListener("click", (e) => {
+	e.preventDefault();
+	// <- Check if the input field is empty, focus if it is ->
+	input.value !== "" ? addTask() : input.focus();
+});
+
+// <--- Debug mode listener --->
+debugButton.addEventListener("click", (e) => {
+	runTests();
+
+	// <- Toggle debug mode ->
+	debugMode = !debugMode;
+
+	// <- Toggle debug button text ->
+	debugButton.innerText = debugMode ? "Debug Mode: On" : "Debug Mode: Off";
+
+	// <- If debug mode is false, clear the list and input ->
+	if (!debugMode) {
+		taskList.innerHTML = "";
+		input.value = "";
+	}
+});
+
+// <====== FACTORIES! ======>
+
+// <--- Factory for buttons --->
+function buttonFactory({ id, text, func }) {
+	const button = document.createElement("button");
+
+	// <- Assign id and text to the button ->
+	[button.id, button.innerText] = [id, text];
+
+	// <- Execute function on the button's parent element ->
+	button.addEventListener("click", (e) => func(e.target.parentElement));
 	return button;
 }
+
+// <--- don't know about this one --->
+// function taskFactory()) {
+//   const task = input.value;
+// 	if (!task) return;
+// 	const listItem = document.createElement("li");
+// 	const listText = document.createElement("p");
+// 	listText.innerText = task;
+// 	listItem.append(listText);
+// 	taskList.append(listItem);
+// 	input.value = "";
+//  return task;
+// }
--- scripts/testing.js
+++ scripts/testing.js
@@ -1,54 +1,93 @@
+// <====== TEST HELPERS ======>
+
 function equal(actual, expected, message) {
 	if (actual === expected) {
-		const defaultMessage = `Expected ${expected} and received ${actual}`;
-		console.info("Pass: " + (message || defaultMessage));
+		console.info("Pass: " + (message || `Expected ${expected} and received ${actual}`));
 	} else {
-		const defaultMessage = `Expected ${expected} but received ${actual} instead`;
-		console.error("Fail: " + (message || defaultMessage));
+		console.error("Fail: " + (message || `Expected ${expected} but received ${actual} instead`));
 	}
 }
 
 function test(name, testFunction) {
 	console.group(name);
 	testFunction();
-	console.groupEnd(name);
+	console.groupEnd();
 }
 
-debugButton.addEventListener("click", (e) => {
-	debugMode;
+// <====== TESTS ======>
+
+function runTests() {
 	const testValue = (input.value = "Test Task");
 	const testTask = addTask(testValue);
 
-	const testValue2 = (input.value = "Test Task 2");
-	const testTask2 = addTask(testValue2);
-
+	// <--- Test 1: addTask function --->
 	test("add Task", () => {
-		test("addTask() returns the added task", () => {
-			const result = testTask;
-			const expected = testValue;
-			equal(result, expected, `addTask returned "${expected}"`);
-		});
-
+		// <- Test 1.1: clear input field ->
 		test("addTask() should clear the input field", () => {
 			const result = input.value;
 			const expected = "";
 			equal(result, expected, "Input field has been cleared");
 		});
 
-		//setTimeout(() => {
+		// <- Test 1.2: add task to list ->
 		test("addTask() should add a new task to the list", () => {
-			const result = taskList.children[0].children[0].innerText;
-			const expected = testValue;
-			equal(result, expected, `"${expected}" task has been added to the list`);
+			const result = taskList.children.length > 0;
+			const expected = true;
+			equal(result, expected, `New task has been added to the list`);
 		});
-		//}, 1000);
 	});
 
-	if (!debugMode) {
-		input.value = "";
-		taskList.innerHTML = "";
-	}
-});
+	// <--- Test 2: button factory --->
+	test("buttonFactory()", () => {
+		// <- Test 2.1: create a button ->
+		test("buttonFactory() should create a button", () => {
+			let result = buttonFactory({ id: "test", text: "Test", func: () => {} });
+			result = result.nodeName;
+			const expected = "BUTTON";
+			equal(result, expected, `Button has been created`);
+		});
+
+		// <- Test 2.2: assign id to button ->
+		// <- Test 2.3: assign text to button ->
+		// <- Test 2.4: assign function to button ->
+	});
+
+	// <--- Test 3: completeTask function --->
+	test("completeTask()", () => {
+		// <- Test 3.1: toggle completed class ->
+		// <- Test 3.2: move task to bottom of list ->
+	});
+
+	// <--- Test 4: urgentTask function --->
+	test("urgentTask()", () => {
+		// <- Test 4.1: toggle urgent class ->
+		// <- Test 4.2: move task to top of list ->
+	});
+
+	// <--- Test 5: editTask function --->
+	test("editTask()", () => {
+		// <- Test 5.1: convert task to input field ->
+		// <- Test 5.2: add save button ->
+		// <- Test 5.3: add cancel button ->
+		// <- Test 5.4: save button should save changes ->
+		// <- Test 5.7: save button should remove save and cancel buttons ->
+		// <- Test 5.5: cancel button should convert input field back to task ->
+		// <- Test 5.6: cancel button should remove save and cancel buttons ->
+	});
+
+	// <--- Test 6: deleteTask function --->
+	test("deleteTask()", () => {
+		// <- Test 6.1: remove task from list ->
+	});
+}
+
+// <====== DEPRECATED TESTS ======>
+
+// test("addTask() returns the added task", () => {
+//   const result = testTask;
+//   const expected = testValue;
+//   equal(result, expected, `addTask returned "${expected}"`);
+// });
 
 // test("Debug Mode", () => {
 // 	equal(debugMode, true, "Debug mode is on");
